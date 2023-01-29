915657e70fc955a0bf1c80c3b1e11418e4049ce3
Manaal Saxena <saxenamanaal@gmail.com>
2023-01-29 21:10:19 +0530

    updated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+
+from urls import url
+
+
+class scraper():
+
+    def __init__(self,position,location):
+        self.position = position
+        self.location = location
+
+    url = url()
+
+    linkedin_url = url.indeed_url
+    indeed_url = url.indeed_url
+    naukri_url = url.naukri_url
+    shine_url = url.shine_url
+
+    def linkedin_data(self):
+
+         
